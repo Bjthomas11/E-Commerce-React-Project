d78bda9e800a5582bed89dfcd2b673988434b355
Brian Thomas <Brianthomas571@gmail.com>
2020-01-03 16:29:30 -0600

    added sign page with styles

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -54,7 +54,7 @@ class SignIn extends Component {
           />
           <div className="buttons">
             <CustomButton type="submit"> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle}>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               Sign in with Google
             </CustomButton>
           </div>
