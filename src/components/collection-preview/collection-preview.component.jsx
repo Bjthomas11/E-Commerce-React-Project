f767885bd41d1fac267acf5245d1205c059e6408
Brian Thomas <Brianthomas571@gmail.com>
2020-01-03 11:15:12 -0600

    adding name and price, styles

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import CollectionItem from "../collection-item/collection-item.component";
+
 import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => (
@@ -7,9 +9,14 @@ const CollectionPreview = ({ title, items }) => (
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
-        .filter((item, index) => index < 4)
-        .map(item => (
-          <div key={item.id}>{item.name}</div>
+        .filter((item, idx) => idx < 4)
+        .map(({ id, name, price, imageUrl }) => (
+          <CollectionItem
+            key={id}
+            name={name}
+            price={price}
+            imageUrl={imageUrl}
+          />
         ))}
     </div>
   </div>
