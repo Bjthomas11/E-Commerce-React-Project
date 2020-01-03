b21e30a16cb88157e1ab65e32b9560aff5712932
Brian Thomas <Brianthomas571@gmail.com>
2020-01-03 10:55:04 -0600

    Adding collections data and methods

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
-import Homepage from "./components/pages/homepage/homepage.component";
+import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
