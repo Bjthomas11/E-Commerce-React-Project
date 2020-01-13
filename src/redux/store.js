fcb2099551692b55e79be7410ff6630ab6f0e215
Brian Thomas <Brianthomas571@gmail.com>
2020-01-13 17:20:15 -0600

    logger only logging in dev

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,17 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 // MIDDLEWARE
+// middleware = fucntions that catch actions and updates the reducer
+// logger = catches the actions and logs the action
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
 // MIDDLEWARE: receive actions in, then do something with them, then pass them out to the Root Reducer
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
